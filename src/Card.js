9a3a9561d7d6527b26785a971697d3db43ba5f51
RiinaYoyo <yoann.forthulman@gmail.com>
2018-01-08 14:16:15 -0400

    Entrée

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 class Card extends Component {
   render() {
     return (
-      <div>
+      <div id="Card">
+          <div>
+                <img src={this.props.src} />
+          </div>
+          <div> 
+                <h1>Un Produit</h1>
+                <p>Une Description</p>
+          </div>
         
       </div>
     );
