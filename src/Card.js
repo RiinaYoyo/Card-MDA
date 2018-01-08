da24f8b13a684069f883c06169a113b96297921e
RiinaYoyo <yoann.forthulman@gmail.com>
2018-01-08 16:10:48 -0400

    ajout fonction afficher cacher

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
+    state={
+        display : "none",
+    }
+    showDesc = () =>{
+        this.setState({
+            display : "block",
+        })
+    }
+    hideDesc = () =>{
+        this.setState({
+            display : "none"
+        })
+    }
   render() {
 
 
@@ -8,11 +21,11 @@ class Card extends Component {
     return (
       <div className="Card">
           <div>
-                <img className="imgProduit" src={this.props.src} />
+                <img className="imgProduit" src={this.props.src} onMouseOver={this.showDesc} onMouseLeave={this.hideDesc} />
           </div>
           <div> 
                 <h1 className="nomProduit" >{this.props.nomProduit}</h1>
-                <p className="descriProduit">{this.props.descriProduit}</p>
+                <p className="descriProduit" style={this.state}>{this.props.descriProduit}</p>
           </div>
       </div>
     );
